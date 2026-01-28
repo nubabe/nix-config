795675355db7713e95c2a0332f468a949c7c1ce8
nubabe <nubabe.me@gmail.com>
2026-01-28 17:25:17 +0100

    Added bootloader

--- hosts/prv3-10/default.nix
+++ hosts/prv3-10/default.nix
@@ -11,5 +11,10 @@
     nubabe.services.tailscale.ipv4 = "100.99.10.2";
     imports = [ /etc/nixos/hardware-configuration.nix ];
     system.stateVersion = "25.05";
+    boot.loader.grub = {
+      enable = true;
+      device = "/dev/sda";
+      useOSProber = true;
+    };
   };
 }
