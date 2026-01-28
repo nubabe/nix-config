a238018d9dcd052278f47bedc0c558e9aeabe3b7
nubabe <nubabe.me@gmail.com>
2026-01-28 17:20:05 +0100

    missing semicolon

--- hosts/prv3-10/default.nix
+++ hosts/prv3-10/default.nix
@@ -9,6 +9,6 @@
   ];
   hostConfig = {
     nubabe.services.tailscale.ipv4 = "100.99.10.2";
-    imports = [ /etc/nixos/hardware-configuration.nix ]
+    imports = [ /etc/nixos/hardware-configuration.nix ];
   };
 }
