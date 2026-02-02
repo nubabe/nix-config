a283d2227a27dcbc2919424f3a6499b9509c9b47
nubabe <nubabe.me@gmail.com>
2026-02-02 21:22:07 +0100

    Error fixed

--- modules/hardware/proxmox-vm.nix
+++ modules/hardware/proxmox-vm.nix
@@ -13,7 +13,7 @@ in
 
 {
 
-  options.nubabe.hardware.proxmox-vm = mkEnableOption "nubabe hardware configuration for a Proxmox VM with UEFI";
+  options.nubabe.hardware.proxmox-vm.enable = mkEnableOption "nubabe hardware configuration for a Proxmox VM with UEFI";
 
   config = mkIf cfg.enable {
 
