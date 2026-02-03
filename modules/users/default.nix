0b1ec95deed84bfc24ff4e188bd5c253361a7855
nubabe <nubabe.me@gmail.com>
2026-02-03 10:44:38 +0100

    Changed initial hashed password to hashed password

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -63,7 +63,7 @@ in
         packages = with pkgs; [ ];
         openssh.authorizedKeys.keys = cfg.authorizedSSHKeys;
         hashedPasswordFile = cfg.hashedPasswordFile;
-        initialHashedPassword = cfg.initialHashedPassword;
+        hashedPassword = cfg.initialHashedPassword;
       };
 
     };
